895f94bd5fe649f594331f1135147def4d10ae80
MagicYyx <493055901@qq.com>
2019-06-20 10:03:24 +0800

    new

--- weibo/weibo/spiders/earthquake.py
+++ weibo/weibo/spiders/earthquake.py
@@ -9,7 +9,7 @@ class EarthquakeSpider(scrapy.Spider):
     allowed_domains = ['m.weibo.cn']
     start_urls = [
         'https://m.weibo.cn/api/container/getIndex?uid=1904228041&luicode=10000011&lfid=100103type%3D3%26q%3D%E5%9C%B0%E9%9C%87%E9%80%9F%E6%8A%A5%26t%3D0&type=uid&value=1904228041&containerid=1076031904228041&page=' +
-        str(i) for i in range(1, 301)
+        str(i) for i in range(1, 10)
     ]
 
     def parse(self, response):
